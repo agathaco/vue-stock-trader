54f4d7a479a84d4daa1fe1713b100a7b34bc8b69
Agathe Cocco <agathecocco@gmail.com>
2018-05-10 15:06:09 +0100

    set up routes

--- src/main.js
+++ src/main.js
@@ -1,17 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import VueRouter from 'vue-router'
+import App from './App.vue'
+import { routes } from './routes'
 import {store} from './store/store'
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
 
-/* eslint-disable no-new */
+const router = new VueRouter({
+  mode:'history',
+  routes
+})
+
 new Vue({
   el: '#app',
   router,
   store,
-  components: { App },
-  template: '<App/>'
+  render: h => h(App)
 })
